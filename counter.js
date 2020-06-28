8213b6a2e6f955f7a6409018d96e9fda11a80b45
elina-wang <siaosyuan0611@gmail.com>
2020-06-28 16:28:13 +0800

    動畫添加

--- counter.js
+++ counter.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
+
+    $(window).scroll(function () {
+
+        // var 變數=資料
+        var offset = $("#test").offset();        // 取得元素位移資料
+        var top = offset.top;                   //上方
+        var left = offset.left;                 //左方
+
+        //console.log("元素的上方：" + top);
+
+        var windowTop = $(window).scrollTop();
+
+        //console.log("視窗的上方：" + windowTop);
+
+        if( windowTop > top) {
+            //console.log("開始動畫...")
+            $("#test").animate({
+            num : 100
+            },{ duration:2500,step:(now) => {
+                $("#test").text(now);
+            }
     
-    // var 變數=資料
-    var offset = $(#test).offset();         // 取得元素位移資料
-    var top = offset.top;                   //上方
-    var left = offset.left;                 //左方
-    
+            })
+        }
+
+
+    });
+
+
+
+
 });
