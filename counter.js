a6755f0fc906f95b7d8d28e714a1dec4d6aba5ea
elina-wang <siaosyuan0611@gmail.com>
2020-06-28 15:29:39 +0800

    頁面準備

---
+++ counter.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    
+    // var 變數=資料
+    var offset = $(#test).offset();         // 取得元素位移資料
+    var top = offset.top;                   //上方
+    var left = offset.left;                 //左方
+    
+});
