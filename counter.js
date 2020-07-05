fe2e4c809551e5ef7d1f61cd79b7a535cacbe2b8
elina-wang <siaosyuan0611@gmail.com>
2020-07-05 10:47:23 +0800

    參數設定及動畫設定

--- counter.js
+++ counter.js
@@ -13,11 +13,23 @@ $(document).ready(function () {
 
         //console.log("視窗的上方：" + windowTop);
 
-        if( windowTop > top) {
+        // attr ("文字")
+        var number = $("#test").attr("data-counter-number");
+        var time = $("#test").attr("data-counter-time");
+        var offset = $("#test").attr("data-counter-offset");
+
+        // 測試動畫
+        //console.log("數字:" + number);
+        //console.log("時間:" + time);
+        //console.log("位移:" + offset);
+
+
+        if( windowTop > top - offset) {
             //console.log("開始動畫...")
+            // duration 需要數字, parsetInt(字串),將字串轉為整數。
             $("#test").animate({
-            num : 100
-            },{ duration:2500,step:(now) => {
+            num : number
+            },{ duration: parseInt(time),step:(now) => {
                 $("#test").text(Math.floor (now));
             }
     
